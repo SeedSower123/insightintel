ea327c0597a31d77c1beea863a849eab4cf8e64d
SeedSower123 <sadlerstewart17@gmail.com>
2025-07-23 15:58:04 +0000

    app

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def index():
 
 # ✅ Run the app
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5050))
     app.run(host='0.0.0.0', port=port)
